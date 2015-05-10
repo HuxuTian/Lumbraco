e6647215cc6fcdd9520ff5bb379aaa245eb07650
HuxuTian <huxu.htian@gmail.com>
2015-05-10 13:21:48 -0400

    Commit omitted files.

---
+++ Lumbraco/Lumbraco.EPiServer.Demo/Models/CustomField.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Core;
+using Lumbraco.IndexService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lumbraco.EPiServer.Demo.Models
+{
+    public class CustomField : IComputedIndexField<ContentData>, IIndexable<ContentData>
+    {
+        public string ComputeFieldValue(ContentData item)
+        {
+            return "CustomFieldTestValue";
+        }
+
+        public Lucene.Net.Documents.Field GetField(ContentData obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
